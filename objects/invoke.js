dee468e0d3d4c8baf2ff783e78c7a3d4cb98ff4c
Evgeniy.Chukreev <dechukor@gmail.com>
2024-11-06 21:04:18 +0500

    objects/invoke.js completed

--- objects/invoke.js
+++ objects/invoke.js
@@ -10,10 +10,20 @@
  */
 
 const invoke = (object, path, func, args) => {
-    throw new Error(`Напишите здесь свое решение ${object} ${path} ${func} ${args}`);
+    // throw new Error(`Напишите здесь свое решение ${object} ${path} ${func} ${args}`);
+    
+    if (typeof(args[0]) === 'string') {
+        return eval(`object.${path}.${func}('${args}')`);
+    } else {
+        return eval(`object.${path}.${func}(${args})`);
+    }
+
+    
 };
 
 const data = {a: {b: [1, 2, 3]}}
 console.log(invoke(data, 'a.b', 'splice', [1, 2])); // [2, 3]
 
 module.exports = invoke;
+
+// РАБОТАЕТ, НО НЕ ИДЕАЛЬНО
