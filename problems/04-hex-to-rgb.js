28f2f4ab0e630b85a05ef3645b304bf8a86a47b8
Evgeniy.Chukreev <dechukor@gmail.com>
2024-11-09 14:50:32 +0500

    problems/04-hex-to-rgb.js completed

--- problems/04-hex-to-rgb.js
+++ problems/04-hex-to-rgb.js
@@ -1,3 +1,4 @@
+//РЕШЕНО
 /**
  * Hex и RGB - текстовые форматы для представления цвета в коде.
  *
@@ -14,7 +15,25 @@
  * @returns {string}
  */
 function hexToRgb(color) {
-    return undefined;
-}
+
+    color = color.replace('#', '');
+    const colorHexArray = [];
+  
+    if (color.length === 3) {
+      colorHexArray[0] = color.slice(0,1) + color.slice(0,1);
+      colorHexArray[1] = color.slice(1,2) + color.slice(1,2);
+      colorHexArray[2] = color.slice(2) + color.slice(2);
+    } else if (color.length === 6) {
+      colorHexArray[0] = color.slice(0,2);
+      colorHexArray[1] = color.slice(2,4);
+      colorHexArray[2] = color.slice(4,6);
+    }
+  
+    colorHexArray.map((item, ind, arr) => {
+      return arr[ind] = +('0x' + item).toString(10);
+    })
+    
+    return (`rgb(${colorHexArray[0]}, ${colorHexArray[1]}, ${colorHexArray[2]})`);
+  }
 
 module.exports = hexToRgb;
