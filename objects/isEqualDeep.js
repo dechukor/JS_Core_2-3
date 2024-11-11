623a280dd5c9f35b28a161406c1e64fa9f5bb8da
Evgeniy.Chukreev <dechukor@gmail.com>
2024-11-11 19:58:10 +0500

    objects/intersectionDeep completed

--- objects/isEqualDeep.js
+++ objects/isEqualDeep.js
@@ -6,16 +6,30 @@
  * @param {Object} secondObj - Объект с любыми значениями
  * @returns {boolean}
  */
+
+function objToString (object) {
+
+    for (key in object) {
+        if (typeof (object[key]) === 'object') {
+            objToString (object[key])
+        } else {
+            return Object.entries(object).flat().join('')
+        }
+            
+    }
+}
+
 const isEqualDeep = (firstObject, secondObject) => {
     // throw new Error(`Напишите здесь свое решение ${element}`);
     if (Object.values(firstObject).length !== Object.values(secondObject).length) {
         return false;
     }
+    
+    
 
 
-
-    console.log(Object.entries(firstObject));
-    console.log(Object.entries(secondObject).flat().join(''));
+    console.log(objToString(firstObject));
+    // console.log(i);
     return Object.entries(firstObject).flat().join('') === Object.entries(secondObject).flat().join('');
     
     
--- problems/05-timeago.js
+++ problems/05-timeago.js
@@ -15,7 +15,37 @@
  * @returns {string}
  */
 function timeago(seconds) {
-    return undefined;
-}
+    let date = "";
+    let del = 0;
+
+    if (seconds < 10) {
+        return "just now";
+    } else if (seconds < 60) {
+        date = "seconds";
+        del = 10;
+    } else if (seconds < 3600) {
+        seconds = seconds > 1800 ? 1800 : seconds;
+        date = "minutes";
+        del = 60;
+    } else if (seconds < 86400) {
+        seconds = seconds > 43200 ? 43200 : seconds;
+        date = "hours";
+        del = 3600;
+    } else if (seconds < 604800) {
+        if (seconds > 172799) return "a few days ago";
+        date = "days";
+        del = 86400;
+    } else if (seconds < 2419200) {
+        date = "weeks";
+        del = 604800;
+    } else {
+        return "undefined";
+    }
+
+    let all = Math.floor(seconds / del);
+
+    return `${date === "seconds" ? (all *= 10) : all} ${
+        all !== 1 ? date : date.slice(0, -1)
+    } ago`;
 
 module.exports = timeago;
